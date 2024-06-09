a7f2cf060981b958fcdbad3df35900b26f922b87
ricardo <ridaoc19@gmail.com>
2024-06-09 16:07:54 +0200

    fix:(style): width info card

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: 'bundle.js',
-		publicPath: "/"
+		publicPath: '/',
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
